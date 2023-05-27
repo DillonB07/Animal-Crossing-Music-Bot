f679bc27a9dca393cc317ccfe5e77442a5fce181
DillonB07 <dillonb07dev@gmail.com>
2023-05-27 20:01:13 +0100

    Update util for Interaction model

--- utils.py
+++ utils.py
@@ -22,27 +22,27 @@ async def create_embed(
     return embed
 
 
-async def handle_error(ctx, error, ephemeral=True):
+async def handle_error(interaction: discord.Interaction, error, ephemeral=True):
     if isinstance(error, commands.CommandOnCooldown):
-        await ctx.reply(
+        await interaction.response.send_message(
             embed=await create_embed(
                 description="You're on cooldown for {:.1f}s".format(error.retry_after),
                 ephemeral=ephemeral,
             )
         )
     elif isinstance(error, commands.DisabledCommand):
-        await ctx.reply(
+        await interaction.response.send_message(
             embed=await create_embed(description="This command is disabled."),
             ephemeral=ephemeral,
         )
     elif isinstance(error, NoVCError):
-        await ctx.reply(
+        await interaction.response.send_message(
             embed=await create_embed(
-                description="I must be in a voice channel to play music and I wasn't able to join your vc."
+                description="I was not able to join your voice channel",
             )
         )
     else:
-        await ctx.reply(
+        await interaction.response.send_message(
             embed=await create_embed(description=error), ephemeral=ephemeral
         )
 
